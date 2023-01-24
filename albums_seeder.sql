1996a1c1f013162d285c3ad7b30f47d6c96e0a9d
Wyatt DeRamus <deramus.wyatt@gmail.com>
2023-01-24 15:24:18 -0600

    Finished the insert portion of the database exercis

---
+++ albums_seeder.sql
@@ -0,0 +1,6 @@
+USE codeup_test_db;
+-- INSERT INTO quotes (author_first_name, author_last_name, content)
+-- VALUES ('Douglas', 'Adams', 'I love deadlines. I love the whooshing noise they make as they go by.');
+INSERT INTO albums (artist, name, release_date, sales, genre)
+VALUES ('Led Zeppelin', 'Led Zeppelin IV', '1971', '30.4', 'Hard rock, heavy metal, folk rock'),
+       ('Michael Jackson', 'Bad', '1987', '21.1', 'Pop, rhythm and blues, funk and rock');
