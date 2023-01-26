7fd5bd4daa8b7bd60270cf8bd7a3718fceb034d8
Wyatt DeRamus <deramus.wyatt@gmail.com>
2023-01-26 16:35:44 -0600

    repository experimenting

--- aliases_exercises.sql
+++ aliases_exercises.sql
@@ -3,4 +3,4 @@ USE employees;
 # Return 10 employees in a result set named 'full_name' in the format of 'last name, first name' for each employee.
 SELECT CONCAT(emp_no, ' ', '-', ' ',last_name, ' ', first_name) AS full_name,
                                                     birth_date AS DOB
-                                                    FROM employees LIMIT 10;
+                                                    FROM employees LIMIT 11;
